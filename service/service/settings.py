d3cf8ff29a3a5f365bc71e242d2a1b9330ab3fdf
otadev <gospodin777@mail.ru>
2026-04-24 23:53:16 +0500

    day #2-part #1

--- service/service/settings.py
+++ service/service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'clients',
+    'services',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,13 @@ WSGI_APPLICATION = 'service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASS'),
+        'HOST': os.getenv('DB_HOST'),
+        # 'PORT': os.getenv('POSTGRES_PORT','5432'),
+        # 'ATOMIC_REQUESTS': True,
     }
 }
 
---
+++ service/services/models.py
@@ -0,0 +1,24 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+from clients.models import Client
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=50)
+    full_price = models.PositiveIntegerField()
+
+class Plan(models.Model):
+    PLAN_TYPES = (
+        ('full', 'Full'),
+        ('student', 'Student'),
+        ('discount', 'Discount')
+    )
+
+    plan_type = models.CharField(max_length=10, choices=PLAN_TYPES)
+    discount_percent = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
+
+class Subscription(models.Model):
+    client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
+    service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
+    plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
